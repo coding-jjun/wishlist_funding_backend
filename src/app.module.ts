3e26e19d25a53a8fced2fe7568cf977633e8b2d9
coding-jjun <psjbrian@gmail.com>
2024-03-05 15:58:25 +0900

    add folder in src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [],
+  imports: [NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
