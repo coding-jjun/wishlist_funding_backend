41d33e94665f7b50648327764fc3dc25380f7291
ChoiWheatley <chltmdgus604@naver.com>
2024-04-24 23:11:51 +0900

    fix(usefaker): 성능향상

--- scripts/usefaker.py
+++ scripts/usefaker.py
@@ -42,109 +42,109 @@ if __name__ == "__main__":
     MAX_IMG_ID = 100 # TODO - SELECT 문으로 직접 알아내기
     MAX_FUND_ID = 100 # TODO - SELECT 문으로 직접 알아내기
 
-    for _ in range(ARG_NUMBER):
 
-        match ARG_TABLE:
-            case "account":
-                kwargs = {
-                    "bank": fake.word(
-                        ext_word_list=[
-                            "Kakaobank",
-                            "Nonghyup",
-                            "Kookmin",
-                            "Shinhan",
-                            "Woori",
-                            "Ibk",
-                            "Hana",
-                            "Daegu",
-                            "Kyongnam",
-                            "Gwangju",
-                            "Busan",
-                            "Jeju",
-                            "Deutschebank",
-                            "Kdb",
-                            "Nfcf",
-                            "Kfcc",
-                            "Suhyup",
-                            "Nacufok",
-                            "Citybank",
-                            "Tossbank",
-                            "Scbank",
-                            "Kbank",
-                        ]
-                    ),
-                    "accNum": fake.credit_card_number(),
-                }
-            case "address":
-                kwargs = {
-                    "userId": fake.random_int(min=1, max=MAX_USER_ID),
-                    "addrRoad": fake.street_address(),
-                    "addrDetl": fake.address(),
-                    "addrZip": fake.postcode(),
-                    "addrNick": fake.emoji(),
-                }
-            case "comment":
-                kwargs = {
-                    "fundId": fake.random_int(min=1, max=MAX_FUND_ID),
-                    "authorId": fake.random_int(min=1, max=MAX_USER_ID),
-                    "content": fake.sentence(),
-                }
-            case "donation":
-                pass
-            case "friend":
-                pass
-            case "funding":
-                kwargs = {
-                    "fundTitle": fake.paragraph(nb_sentences=1),
-                    "fundCont" : fake.paragraph(nb_sentences=2),
-                    "fundTheme" : fake.word(ext_word_list=["Birthday", "Anniversary", "Donation"]),
-                    "fundGoal" : fake.random_int(min=1000, max=100_000_000),
-                    "endAt" : fake.future_date(end_date=date(2055, 3, 29)).strftime("%Y-%m-%d"),
-                    "fundUser" : fake.random_int(min=MIN_USER_ID, max=MAX_USER_ID),
-                }
-            case "gratitude":
-                pass
-            case "image":
-                kwargs = {
-                    "imgUrl": "https://picsum.photos/200",
-                    "imgType": fake.word(
-                        ext_word_list=[
-                            "Funding",
-                            "Donation",
-                            "Gratitude",
-                            "RollingPaper",
-                        ]
-                    ),
-                    "subId": fake.random_int(min=1, max=100),
-                }
-            case "notification":
-                pass
-            case "open_bank_token":
-                pass
-            case "rolling_paper":
-                pass
-            case "user":
-                kwargs = {
-                    "userNick": fake.safe_color_name()
-                    + " "
-                    + fake.first_name()
-                    + " "
-                    + str(fake.random_int()),
-                    "userPw": fake.password(),
-                    "userName": fake.name(),
-                    "userPhone": fake.phone_number(),
-                    "userEmail": fake.email(),
-                    "userBirth": fake.date(),
-                    "accId": fake.random_int(min=1, max=MAX_USER_ID),
-                    "userImg": fake.random_int(min=1, max=MAX_ACC_ID),
-                }
+    with psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_DEV_USERNAME"),
+        password=os.getenv("DB_DEV_PASSWORD"),
+        dbname=os.getenv("DB_DEV_DATABASE"),
+    ) as conn:
+        for _ in range(ARG_NUMBER):
+            match ARG_TABLE:
+                case "account":
+                    kwargs = {
+                        "bank": fake.word(
+                            ext_word_list=[
+                                "Kakaobank",
+                                "Nonghyup",
+                                "Kookmin",
+                                "Shinhan",
+                                "Woori",
+                                "Ibk",
+                                "Hana",
+                                "Daegu",
+                                "Kyongnam",
+                                "Gwangju",
+                                "Busan",
+                                "Jeju",
+                                "Deutschebank",
+                                "Kdb",
+                                "Nfcf",
+                                "Kfcc",
+                                "Suhyup",
+                                "Nacufok",
+                                "Citybank",
+                                "Tossbank",
+                                "Scbank",
+                                "Kbank",
+                            ]
+                        ),
+                        "accNum": fake.credit_card_number(),
+                    }
+                case "address":
+                    kwargs = {
+                        "userId": fake.random_int(min=1, max=MAX_USER_ID),
+                        "addrRoad": fake.street_address(),
+                        "addrDetl": fake.address(),
+                        "addrZip": fake.postcode(),
+                        "addrNick": fake.emoji(),
+                    }
+                case "comment":
+                    kwargs = {
+                        "fundId": fake.random_int(min=1, max=MAX_FUND_ID),
+                        "authorId": fake.random_int(min=1, max=MAX_USER_ID),
+                        "content": fake.sentence(),
+                    }
+                case "donation":
+                    pass
+                case "friend":
+                    pass
+                case "funding":
+                    kwargs = {
+                        "fundTitle": fake.paragraph(nb_sentences=1),
+                        "fundCont" : fake.paragraph(nb_sentences=2),
+                        "fundTheme" : fake.word(ext_word_list=["Birthday", "Anniversary", "Donation"]),
+                        "fundGoal" : fake.random_int(min=1000, max=100_000_000),
+                        "endAt" : fake.future_date(end_date=date(2055, 3, 29)).strftime("%Y-%m-%d"),
+                        "fundUser" : fake.random_int(min=MIN_USER_ID, max=MAX_USER_ID),
+                    }
+                case "gratitude":
+                    pass
+                case "image":
+                    kwargs = {
+                        "imgUrl": "https://picsum.photos/200",
+                        "imgType": fake.word(
+                            ext_word_list=[
+                                "Funding",
+                                "Donation",
+                                "Gratitude",
+                                "RollingPaper",
+                            ]
+                        ),
+                        "subId": fake.random_int(min=1, max=100),
+                    }
+                case "notification":
+                    pass
+                case "open_bank_token":
+                    pass
+                case "rolling_paper":
+                    pass
+                case "user":
+                    kwargs = {
+                        "userNick": fake.safe_color_name()
+                        + " "
+                        + fake.first_name()
+                        + " "
+                        + str(fake.random_int()),
+                        "userPw": fake.password(),
+                        "userName": fake.name(),
+                        "userPhone": fake.phone_number(),
+                        "userEmail": fake.email(),
+                        "userBirth": fake.date(),
+                        "accId": fake.random_int(min=1, max=MAX_USER_ID),
+                        "userImg": fake.random_int(min=1, max=MAX_ACC_ID),
+                    }
 
-        with psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_DEV_USERNAME"),
-            password=os.getenv("DB_DEV_PASSWORD"),
-            dbname=os.getenv("DB_DEV_DATABASE"),
-        ) as conn:
             exec_insertion(conn, ARG_TABLE, **kwargs)
 
         conn.commit()
