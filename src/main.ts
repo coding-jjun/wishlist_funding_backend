2ffd7d452e0e5008e396b6feebf3805ad9afa640
hjlee5701 <hjlee5701@gmail.com>
2024-07-28 21:36:18 +0900

    fix: delete duplicate CORS configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ async function bootstrap() {
     origin: 'https://www.giftogether.co.kr', // 쿠키가 설정될 도메인
     credentials: true // 자격 증명이 포함된 요청을 허용
   });
-  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
