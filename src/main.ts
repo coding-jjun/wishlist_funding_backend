7c7bd0ddf104cc5158d4b957e074122b62af048d
hjlee5701 <hjlee5701@gmail.com>
2024-07-28 21:15:11 +0900

    fix: cookie option and cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser());
+  app.enableCors({
+    origin: 'https://www.giftogether.co.kr', // 쿠키가 설정될 도메인
+    credentials: true // 자격 증명이 포함된 요청을 허용
+  });
   app.enableCors();
   await app.listen(port);
 }
