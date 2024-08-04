612830b13d494be1fa8fc5e8874469255a384a50
hjlee5701 <hjlee5701@gmail.com>
2024-08-04 21:03:09 +0900

    feat: add .env for CORS origin and cookie domain

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -36,7 +36,7 @@ export class AuthController {
     path: '/',
     secure: true,
     // sameSite: 'none' as 'none', // 크로스 도메인 요청을 허용하기 위해 none으로 설정
-    domain: '.giftogether.co.kr' // 애플리케이션 도메인으로 설정
+    domain: process.env.COOKIE_DOMAIN // 애플리케이션 도메인으로 설정
   };
 
   @Get('kakao')
