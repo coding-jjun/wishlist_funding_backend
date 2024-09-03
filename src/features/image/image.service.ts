a7c6133792706231676d1734a613e197e4d9b999
ChoiWheatley <chltmdgus604@naver.com>
2024-09-03 23:34:10 +0900

    ✨ feat(image): Add unlink for Image Service

--- src/features/image/image.service.ts
+++ src/features/image/image.service.ts
@@ -7,6 +7,10 @@ import {
   S3Client,
 } from '@aws-sdk/client-s3';
 import { GiftogetherExceptions } from 'src/filters/giftogether-exception';
+import { ImageType } from 'src/enums/image-type.enum';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Image } from 'src/entities/image.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ImageService {
@@ -17,7 +21,10 @@ export class ImageService {
 
   private readonly bucketName = process.env.AWS_S3_BUCKET_NAME;
 
-  constructor(private readonly g2gException: GiftogetherExceptions) {}
+  constructor(
+    private readonly g2gException: GiftogetherExceptions,
+    @InjectRepository(Image) private readonly imgRepo: Repository<Image>,
+  ) {}
 
   getObjectUrlOf(filename: string): string {
     return `https://${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${filename}`;
@@ -35,6 +42,25 @@ export class ImageService {
     return this.getObjectUrlOf(filename);
   }
 
+  /**
+   * Image Table에서 레코드 하나를 제거하는 메서드. ImageService를 Inject하여 사용하세요.
+   * @param imgType 연관 테이블 타입
+   * @param subId FK
+   */
+  async unlink(imgType: ImageType, subId: number) {
+    const image = await this.imgRepo.findOne({
+      where: { imgType, subId },
+    });
+    if (!image) {
+      throw this.g2gException.ImageNotFound;
+    }
+    this.imgRepo.remove(image); // TODO - Soft Delete
+  }
+
+  /**
+   * S3 버킷에 저장된 객체를 삭제합니다.
+   * @param url 삭제하고자 하는 S3 기프투게더 이미지 URL
+   */
   async delete(url: string): Promise<void> {
     const regex = /^(https?):\/\/([^\/]+)\/(.*)?$/;
     const matches = url.match(regex);
