edc224f8b5b45d5b61e33ac45d1d002fe0810244
ChoiWheatley <chltmdgus604@naver.com>
2024-09-30 23:35:35 +0900

    🏷️ misc(image): nullable column

--- src/entities/image.entity.ts
+++ src/entities/image.entity.ts
@@ -22,16 +22,16 @@ export class Image {
     enum: ImageType,
     nullable: true,
   })
-  imgType: ImageType;
+  imgType?: ImageType;
 
   @Index({ unique: false })
   @Column('int', { nullable: true })
-  subId: number;
+  subId?: number;
 
   @ManyToOne(() => User, (user) => user.createdImages)
   creator: User;
 
-  constructor(imgUrl: string, imgType: ImageType, subId: number) {
+  constructor(imgUrl: string, imgType?: ImageType, subId?: number) {
     this.imgUrl = imgUrl;
     this.imgType = imgType;
     this.subId = subId;
