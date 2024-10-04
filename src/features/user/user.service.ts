fcb6f5a11905eb5bf41bcceb7a1ee3d69284a27e
ChoiWheatley <chltmdgus604@naver.com>
2024-10-05 02:00:09 +0900

    ♻️ refactor(user): no dependency to image repository

--- src/features/user/user.service.ts
+++ src/features/user/user.service.ts
@@ -3,8 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Account } from 'src/entities/account.entity';
-import { Image } from 'src/entities/image.entity';
 import { GiftogetherExceptions } from 'src/filters/giftogether-exception';
 import { ImageType } from 'src/enums/image-type.enum';
 import { UserDto } from './dto/user.dto';
@@ -17,22 +15,18 @@ export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-    @InjectRepository(Account)
-    private readonly accRepository: Repository<Account>,
-    @InjectRepository(Image)
-    private readonly imgRepository: Repository<Image>,
-
     private readonly g2gException: GiftogetherExceptions,
-    
+
     private readonly imgService: ImageService,
   ) {}
 
   // 사용자 정보 조회
   async getUserInfo(user: User): Promise<UserDto> {
-    const where = user.defaultImgId
-      ? { imgId: user.defaultImgId }
-      : { imgType: ImageType.User, subId: user.userId };
-    const image = await this.imgRepository.findOne({ where });
+    const image = user.defaultImgId
+      ? await this.imgService.getInstanceByPK(user.defaultImgId)
+      : (
+          await this.imgService.getInstancesBySubId(ImageType.User, user.userId)
+        )[0];
 
     return new UserDto(
       user.userNick,
@@ -52,10 +46,11 @@ export class UserService {
   async getOthersInfo(userId: number): Promise<UserDto> {
     const user = await this.userRepository.findOne({ where: { userId } });
 
-    const where = user.defaultImgId
-      ? { imgId: user.defaultImgId }
-      : { imgType: ImageType.User, subId: user.userId };
-    const image = await this.imgRepository.findOne({ where });
+    const image = user.defaultImgId
+      ? await this.imgService.getInstanceByPK(user.defaultImgId)
+      : (
+          await this.imgService.getInstancesBySubId(ImageType.User, user.userId)
+        )[0];
 
     return new UserDto(
       user.userNick,
@@ -98,46 +93,36 @@ export class UserService {
 
       user.defaultImgId = null;  // 기본 이미지 ID를 null로 설정
       imageUrl = savedImage.imgUrl;  // 이미지 URL 설정
-    } else if (defaultImgId) { // 기본 이미지가 제공된 경우
+    } else if (defaultImgId) {
+      // 기본 이미지가 제공된 경우
       // 기본 이미지 ID가 유효한지 확인
       if (!DefaultImageIds.User.includes(defaultImgId)) {
         throw this.g2gException.DefaultImgIdNotExist;
       }
-  
+
       // 기존 사용자 지정 프로필 이미지가 있는 경우 삭제
       if (!user.defaultImgId) {
         await this.imgService.delete(ImageType.User, userId);
       }
-  
-      const defaultImage = await this.imgRepository.findOne({
-        where: { imgId: defaultImgId },
-      });
-  
+
+      const defaultImage = await this.imgService.getInstanceByPK(defaultImgId);
+
       if (defaultImage) {
-        user.defaultImgId = defaultImage.imgId;  // 기본 이미지 ID 설정
-        imageUrl = defaultImage.imgUrl;  // 이미지 URL 설정
+        user.defaultImgId = defaultImage.imgId; // 기본 이미지 ID 설정
+        imageUrl = defaultImage.imgUrl; // 이미지 URL 설정
       } else {
         throw this.g2gException.DefaultImgIdNotExist;
       }
     } else { // 기본 이미지나 사용자 지정 이미지가 제공되지 않은 경우
-      if (user.defaultImgId) {
-        const image = await this.imgRepository.findOne({
-          where: { imgId: user.defaultImgId },
-        });
-        if (image) {
-          imageUrl = image.imgUrl;  // 기존 기본 이미지의 URL 설정
-        }
-      } else {
-        const image = await this.imgRepository.findOne({
-          where: { 
-            imgType: ImageType.User,
-            subId: userId,
-          },
-        });
-        if (image) {
-          imageUrl = image.imgUrl;  // 기존 사용자 지정 이미지의 URL 설정
-        }
-      }
+      const image = user.defaultImgId
+        ? await this.imgService.getInstanceByPK(user.defaultImgId)
+        : (
+            await this.imgService.getInstancesBySubId(
+              ImageType.User,
+              user.userId,
+            )
+          )[0];
+      imageUrl = image ? image.imgUrl : null;
     }
   
     // 3. 비밀번호 해시 처리
@@ -169,12 +154,10 @@ export class UserService {
     await this.userRepository.softDelete(user.userId);
 
     const image = user.defaultImgId
-      ? await this.imgRepository.findOne({
-          where: { imgId: user.defaultImgId },
-        })
-      : await this.imgRepository.findOne({
-          where: { imgType: ImageType.User, subId: user.userId },
-        });
+      ? await this.imgService.getInstanceByPK(user.defaultImgId)
+      : (
+          await this.imgService.getInstancesBySubId(ImageType.User, user.userId)
+        )[0];
 
     return new UserDto(
       user.userNick,
