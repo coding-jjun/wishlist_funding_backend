cd136434dbfb4cb79e49dd6d743370574f456d17
ChoiWheatley <chltmdgus604@naver.com>
2024-10-14 17:56:01 +0900

    ➖ dep(valid-check): remove unused dependency

--- src/util/valid-check.module.ts
+++ src/util/valid-check.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ValidCheck } from './valid-check';
 import { GiftogetherExceptions } from 'src/filters/giftogether-exception';
-import { ConfigService } from '@nestjs/config';
 import { CustomUrlValidator } from './custom-url-validator';
 
 @Module({
-  providers: [
-    ValidCheck,
-    GiftogetherExceptions,
-    ConfigService,
-    CustomUrlValidator,
-  ],
+  providers: [ValidCheck, GiftogetherExceptions, CustomUrlValidator],
   exports: [ValidCheck, CustomUrlValidator],
 })
 export class ValidCheckModule {}
