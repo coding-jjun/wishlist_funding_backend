e5aa9d213c3ec8cdfe08756672d3071600a23fd5
ChoiWheatley <chltmdgus604@naver.com>
2024-11-30 21:41:46 +0900

    chore: Remove unused imports, Logger

--- src/features/comment/comment.service.ts
+++ src/features/comment/comment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { Comment } from 'src/entities/comment.entity';
@@ -13,8 +13,7 @@ import { ValidCheck } from 'src/util/valid-check';
 
 function convertToGetCommentDto(comment: Comment): GetCommentDto {
   const { comId, content, regAt, isMod, authorId, author } = comment;
-  Logger.log(`comment: ${JSON.stringify(comment)}`);
-  const authorName = author?.userName || 'ANONYMOUS';
+  const authorName = author?.userName ?? 'ANONYMOUS';
   return new GetCommentDto(comId, content, regAt, isMod, authorId, authorName);
 }
 
