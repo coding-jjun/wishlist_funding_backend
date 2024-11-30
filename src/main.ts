c6be1113c759eb0b7d99090d55482e0455492704
ChoiWheatley <chltmdgus604@naver.com>
2024-11-30 23:15:39 +0900

    feat(cors): change cors origin policy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as fs from 'fs';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const port = process.env.PORT;
@@ -33,11 +34,21 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, nestFactoryOptions);
 
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser());
   app.enableCors({
-    origin: process.env.CORS_ORIGIN, // 쿠키가 설정될 도메인
-    credentials: true // 자격 증명이 포함된 요청을 허용
+    origin: (origin, callback) => {
+      const allowedOrigins = configService
+        .get<string>('CORS_ORIGIN')
+        .split(',');
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // 자격 증명이 포함된 요청을 허용
   });
   await app.listen(port);
 }
