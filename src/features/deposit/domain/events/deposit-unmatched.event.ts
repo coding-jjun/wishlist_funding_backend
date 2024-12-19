66a218158cb25c58437cb9a663f521241a31fa47
ChoiWheatley <chltmdgus604@naver.com>
2024-12-19 14:13:34 +0900

    feat(deposit.unmatched): Add two additional events

--- src/features/deposit/domain/events/deposit-unmatched.event.ts
+++ src/features/deposit/domain/events/deposit-unmatched.event.ts
@@ -1,5 +1,9 @@
 import { Deposit } from 'src/features/deposit/domain/entities/deposit.entity';
+import { ProvisionalDonation } from '../entities/provisional-donation.entity';
 
 export class DepositUnmatchedEvent {
-  constructor(public readonly deposit: Deposit) {}
+  constructor(
+    public readonly deposit: Deposit,
+    public readonly provisionalDonation: ProvisionalDonation,
+  ) {}
 }
